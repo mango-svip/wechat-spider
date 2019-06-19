712dcbfaba6c0a96963b780a0de8c1493fdd81f6
mango-pc <mango@mango-pcdeMacBook-Pro.local>
2019-06-20 01:44:01 +0800

    init spider

---
+++ dto/wechatInfo.go
@@ -0,0 +1,10 @@
+package dto
+
+
+type WeiChatInfo struct {
+    Name string         `json:"name"`
+    Account string      `json:"account"`
+    Description string  `json:"description"`
+    MonthArticle int    `json:"monthArticle"`
+}
+
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mango-svip/wechat-spider
+
+go 1.12
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da
+)
